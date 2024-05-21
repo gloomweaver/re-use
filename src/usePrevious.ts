6d1a329ad43b37e1b9d6d442462ad099a9891148
Kirill Khramkov <hramkir@gmail.com>
2024-05-21 21:17:41 +0400

    Add docs

--- src/usePrevious.ts
+++ src/usePrevious.ts
@@ -1,5 +1,11 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * Returns the previous value of the given value.
+ * @param value The value to track.
+ * @param initialValue The initial value to return if the previous value is not available.
+ * @returns The previous value of the given value.
+ */
 export function usePrevious<T>(value: T): T | undefined;
 export function usePrevious<T>(value: T, initialValue: T): T;
 export function usePrevious<T>(value: T, initialValue?: T) {
